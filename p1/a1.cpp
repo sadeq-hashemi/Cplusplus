20f40ee7568aaa5bb98797061ca9f334e18bd60f
sadeq-hashemi <hasheminejad@wisc.edu>
2018-02-11 22:08:43 -0600

    program done

--- p1/a1.cpp
+++ p1/a1.cpp
@@ -53,12 +53,16 @@ void compare_lists(list<string>& l1, list<string>& l2,
     l2.pop_front();
     all.push_front(name);
   }
+
+  all.sort();
+  shared.sort();
 }
 
 int main(){
 
-string filename1, filename2; 
+string filename1, filename2, name; 
 ifstream file1, file2;
+ofstream all, common;
 list<string>  list1, list2, either, both;
 //prompt for 2 input files
 // Enter the name of the first file: ....
@@ -83,7 +87,7 @@ list<string>  list1, list2, either, both;
   do {
     cout << "Enter the name of the second file: ";
     getline(cin, filename2); //store name
-    file2.open(filename1.c_str()); //start stream to read
+    file2.open(filename2.c_str()); //start stream to read
     valid = file2.good(); //validity of name
     if(!valid) 
       cout << "Input file " << filename2 << " is NOT found. Please try again" << endl;  
@@ -96,18 +100,54 @@ list<string>  list1, list2, either, both;
   get_students(file2, list2);
   
 //output # of students in each: "Number of students in <file1> = <n1>"
-  cout << "Number of students in "<< filename1 << " = " << list1.size() << endl; 
+  cout << endl << "Number of students in "<< filename1 << " = " << list1.size() << endl; 
   cout << "Number of students in "<< filename2 << " = " << list2.size() << endl; 
 //output: # of students that are present in both/either <file1> and <file2> = <n2>
   compare_lists(list1, list2, either, both);
-  cout << "Number of students that are present in both  "<< filename1 << " and "
+  cout << "Number of students that are present in BOTH "<< filename1 << " AND "
        << filename2 << " = " << both.size() << endl; 
-  cout << "Number of students in either "<< filename1 << " or " 
+  cout << "Number of students in EITHER "<< filename1 << " OR " 
        << filename2 << " = " << either.size() << endl; 
 // also store in 2 output files named intersection.txt and union.txt
+  all.open("union.txt");
+  common.open("intersection.txt");
 
+  int bothSize = both.size(); //store sizes before removing
+  int eitherSize = either.size(); //store sizes before removing
+   
+  if (bothSize == 0) {
+    cout << endl << "The file intersection.txt is EMPTY since there are no students that are present in BOTH  "
+    << filename1 << " AND " << filename2 << endl; 
+
+  } else {
+      cout << endl << "List of students that are present in BOTH " << 
+       filename1 << " AND " << filename2 << ":" << endl;
+
+     while(!both.empty()) {
+      common << both.front() << endl; 
+      cout << both.front() << endl;
+      both.pop_front();    
+    }
+
+    cout << endl << "The names of the " << bothSize << " students that are present in BOTH " 
+       << filename1 << " AND " << filename2 << " are written to the file named intersection.txt" 
+       << endl; 
+  }
+  while(!either.empty()) {
+    all << either.front() << endl; 
+    either.pop_front();
+  }
+  if (eitherSize == 0)
+   cout  << "The file union.txt is EMPTY since there are no students in the 2 files "
+   << filename1 << " AND " << filename2 << endl; 
+  else
+    cout << "The names of the " << eitherSize << " students that are present in EITHER " 
+       << filename1 << " OR " << filename2 << " are written to the file named union.txt" 
+       << endl; 
   file1.close();
   file2.close();
+  all.close();
+  common.close();
   return 0;
 }
 
